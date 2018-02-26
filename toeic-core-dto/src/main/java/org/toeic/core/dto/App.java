fc6fd4cf7a0c0d6db3e6c916b6361aab1b892f4e
DavisNguyen <nguyendat111195@gmail.com>
2018-02-26 15:04:41 +0700

    Create new Business logic

---
+++ toeic-core-dto/src/main/java/org/toeic/core/dto/App.java
@@ -0,0 +1,13 @@
+package org.toeic.core.dto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
