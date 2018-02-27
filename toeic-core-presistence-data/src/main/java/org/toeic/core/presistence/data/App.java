2b4ee0b6035fa4e3b70942b47cdc7e3a5f1e88e5
DavisNguyen <nguyendat111195@gmail.com>
2018-02-27 20:42:34 +0700

    Create new toeic-core-common, toeic-core-presistence-data, folder ApplicationFile

---
+++ toeic-core-presistence-data/src/main/java/org/toeic/core/presistence/data/App.java
@@ -0,0 +1,13 @@
+package org.toeic.core.presistence.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
