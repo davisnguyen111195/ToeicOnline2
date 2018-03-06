c24ed478ccfc0ea477468dbcca5ef1adee11bc9e
DavisNguyen <nguyendat111195@gmail.com>
2018-03-06 10:49:38 +0700

    Add new library and anotation

---
+++ toeic-core-presistence-data/src/main/java/vn/myclass/core/persistence/entity/Role.java
@@ -0,0 +1,37 @@
+package vn.myclass.core.persistence.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role {
+	@Id
+	@Column(name = "roleId")
+	private Integer roleId;
+	
+	@Column(name = "roleName")
+	private String roleName;
+	
+	@OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+	private List<Users> userList;
+	
+	public Integer getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Integer roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
